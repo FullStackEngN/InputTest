fafdf2866c81b024a2b4d6b245c994901391dee3
FullStackEngN <fullstackeng@outlook.com>
2021-03-31 11:33:22 +0800

    update code using random string

--- input_test.py
+++ input_test.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pyautogui
+import random
 
 screenWidth, screenHeight = pyautogui.size()
 print("Screen width: " + str(screenWidth) + "; height: " + str(screenHeight))
@@ -25,11 +26,18 @@ pyautogui.click()
 currentMouseX, currentMouseY = pyautogui.position()
 print("Current mouse position X: " + str(currentMouseX) + "; Y: " + str(currentMouseY))
 
+mylist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 x = 0
 while x < 1000:
     # type with quarter-second pause in between each key
-    pyautogui.typewrite('nihao2', interval=0.25)
-    pyautogui.typewrite('jintiantianqizenmeyang ', interval=0.25)
+    #pyautogui.typewrite('nihao2', interval=0.25)
+
+    char1 =random.choice(mylist)
+    char2 =random.choice(mylist)
+    char3 =random.choice(mylist)
+
+    pyautogui.typewrite(char1 + char2 + char3 + ' ', interval=0.25)
 
     x += 1
 
